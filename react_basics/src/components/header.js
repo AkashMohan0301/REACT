62708d8a070ac21b248d85aa07afb5301290e1bb
AkashMohan0301 <akashmohan940@gmail.com>
2025-03-13 13:57:10 +0530

    Add styling to Header component with background and logo customization

--- react_basics/src/components/header.js
+++ react_basics/src/components/header.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
 
+let styles ={
+  header:{
+    background: 'lightblue',
+
+  },
+  logo:{
+    color: 'black',
+    fontFamily: 'Anton',
+    textAlign: 'center',
+    padding: '10px'
+  }
+}
+
 class header extends Component{
+
   render(){
     return (
       <div>
-        <header>
-          <div>
+        <header style={styles.header}>
+          <div style={styles.logo}>
             Logo
           </div>
           <input type="text" />
