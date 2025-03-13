58ca231379ab76b0cf603b7dfc905bd3bb14d245
AkashMohan0301 <akashmohan940@gmail.com>
2025-03-14 00:07:45 +0530

    Update inputChangeHandler to log additional name parameter from input change event

--- react_basics/src/components/header.js
+++ react_basics/src/components/header.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react'
 
 class header extends Component{
 
-  inputChangeHandler (event){
+  inputChangeHandler (event,name){
     console.log(event.target.value);
+    console.log(name);
+
   }
 
   render(){
@@ -14,7 +16,7 @@ class header extends Component{
           <div className="logo">
             Logo
           </div>
-          <input type="text" onChange={this.inputChangeHandler}/>
+          <input type="text" onChange={(e) => this.inputChangeHandler(e,'Akash')}/>
         </header>
       </div>
     )
