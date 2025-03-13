bf2fdf99593c00947d6dcd9f99adca6bda2da635
AkashMohan0301 <akashmohan940@gmail.com>
2025-03-13 14:03:57 +0530

    Refactor Header component to use external CSS for styling and improve layout

--- react_basics/src/components/header.js
+++ react_basics/src/components/header.js
@@ -1,25 +1,12 @@
 import React, { Component } from 'react'
 
-let styles ={
-  header:{
-    background: 'lightblue',
-
-  },
-  logo:{
-    color: 'black',
-    fontFamily: 'Anton',
-    textAlign: 'center',
-    padding: '10px'
-  }
-}
-
 class header extends Component{
 
   render(){
     return (
       <div>
-        <header style={styles.header}>
-          <div style={styles.logo}>
+        <header >
+          <div className="logo">
             Logo
           </div>
           <input type="text" />
